3e4e1eea61cd9bfbee028b878fdcd09d8dd20de0
sm <han220173@gmal.com>
2025-06-18 18:49:11 +0700

    finish form styles

--- therapy/core/forms.py
+++ therapy/core/forms.py
@@ -1,91 +1,91 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordResetForm, SetPasswordForm
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    PasswordResetForm,
+    SetPasswordForm,
+)
 
 User = get_user_model()
 
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(max_length=150, required=True,
-                               label='Имя пользователя или email',
-                               widget=forms.TextInput(
-                                   attrs={'class': 'my_input', 'readonly onfocus': "this.removeAttribute('readonly');",
-                                          'autocomplete': 'off'}))
+    username = forms.CharField(
+        max_length=150,
+        required=True,
+        label="Имя пользователя или email",
+        widget=forms.TextInput(
+            attrs={
+                "class": "my_input",
+                "readonly onfocus": "this.removeAttribute('readonly');",
+                "autocomplete": "off",
+            }
+        ),
+    )
 
-    password = forms.CharField(required=True, label='Пароль',
-                               widget=forms.PasswordInput)
+    password = forms.CharField(
+        required=True, label="Пароль", widget=forms.PasswordInput
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ("username", "password")
 
 
 class RegisterUserForm(UserCreationForm):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["last_name"].required = False
-        
     username = forms.CharField(
         max_length=150,
         required=True,
-        label='Имя пользователя',
-        widget=forms.TextInput(attrs={
-            'readonly': True,
-            'onfocus': "this.removeAttribute('readonly')",
-            'autocomplete': 'off',
-            
-        })
+        label="Имя пользователя",
+        widget=forms.TextInput(
+            attrs={
+                "readonly": True,
+                "onfocus": "this.removeAttribute('readonly')",
+                "autocomplete": "off",
+            }
+        ),
     )
 
-    email = forms.EmailField(
-        required=True,
-        label='Email',
-        widget=forms.TextInput
-    )
+    email = forms.EmailField(required=True, label="Email", widget=forms.TextInput)
 
-    first_name = forms.CharField(
-        required=True,
-        label='Имя',
-        widget=forms.TextInput
-    )
+    first_name = forms.CharField(required=True, label="Имя", widget=forms.TextInput)
 
-    last_name = forms.CharField(
-        required=True,
-        label='Фамилия',
-        widget=forms.TextInput
-    )
+    last_name = forms.CharField(required=False, label="Фамилия", widget=forms.TextInput)
 
     sex = forms.IntegerField(
-        widget=forms.RadioSelect(choices=User.SEX, attrs={'class': 'radio-select'}),
-        label='Пол'
+        widget=forms.RadioSelect(choices=User.SEX, attrs={"class": "radio-select"}),
+        label="Пол",
     )
 
-    phone = forms.CharField(
-        required=True,
-        label='Телефон',
-        widget=forms.TextInput
-    )
+    phone = forms.CharField(required=False, label="Телефон", widget=forms.TextInput)
 
     password1 = forms.CharField(
-        required=True,
-        label='Пароль',
-        widget=forms.PasswordInput
+        required=True, label="Пароль", widget=forms.PasswordInput
     )
 
     password2 = forms.CharField(
-        required=True,
-        label='Повторите пароль',
-        widget=forms.PasswordInput
+        required=True, label="Повторите пароль", widget=forms.PasswordInput
     )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'phone', 'sex', 'photo', 'password1', 'password2',)
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "phone",
+            "sex",
+            "photo",
+            "password1",
+            "password2",
+        )
 
 
 class UserPasswordResetForm(PasswordResetForm):
-    email = forms.EmailField(required=True, label='Email', widget=forms.TextInput)
+    email = forms.EmailField(required=True, label="Email", widget=forms.TextInput)
 
 
 class UserPasswordResetConfirmForm(SetPasswordForm):
@@ -93,13 +93,9 @@ class UserPasswordResetConfirmForm(SetPasswordForm):
         super().__init__(user, *args, **kwargs)
 
     new_password1 = forms.CharField(
-        required=True,
-        label='Новый пароль',
-        widget=forms.PasswordInput
+        required=True, label="Новый пароль", widget=forms.PasswordInput
     )
 
     new_password2 = forms.CharField(
-        required=True,
-        label='Повторите пароль',
-        widget=forms.PasswordInput
+        required=True, label="Повторите пароль", widget=forms.PasswordInput
     )
