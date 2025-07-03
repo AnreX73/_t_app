63b132dfe5e61203000b289f6e394931584b7ddc
sm <han220173@gmal.com>
2025-07-03 18:43:21 +0700

    add  1 model

--- therapy/core/admin.py
+++ therapy/core/admin.py
@@ -1,5 +1,48 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, WorkerSchedule
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "username",
+        "email",
+        "sex",
+        "role",
+        "phone",
+        "photo",
+        "last_login",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "registration_date",
+    )
+    list_display_links = ('id',"first_name","username",)
+    
+    list_filter = (
+        "last_login",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "date_joined",
+        "registration_date",
+    )
+    raw_id_fields = ("groups", "user_permissions")
+    save_on_top = True
+
+
+@admin.register(WorkerSchedule)
+class WorkerScheduleAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "worker",
+        "day_of_week",
+        "start_time",
+        "end_time",
+        "appointment_duration",
+    )
+    list_filter = ("worker",)
--- therapy/core/models.py
+++ therapy/core/models.py
@@ -1,30 +1,75 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class User(AbstractUser):
     SEX = (
-        (1, 'Мужской'),
-        (2, 'Женский'),
+        (1, "Мужской"),
+        (2, "Женский"),
     )
 
     class Role(models.IntegerChoices):
-        Admin = 1, 'Администратор'
-        Worker = 2, 'Специалист'
-        Customer = 3, 'Пользователь'
+        Admin = 1, "Администратор"
+        Worker = 2, "Специалист"
+        Customer = 3, "Пользователь"
 
-    email = models.EmailField(unique=True, verbose_name='email address')
-    sex = models.PositiveSmallIntegerField(choices=SEX, default=1, verbose_name='Пол')
-    role = models.PositiveSmallIntegerField(choices=Role.choices, default=Role.Customer, verbose_name='кто по жизни')
-    phone = models.CharField(max_length=25, blank=True, null=True, verbose_name='Телефон')
-    photo = models.ImageField(upload_to='user.photos', blank=True, null=True, verbose_name='Фото')
-    registration_date = models.DateField(auto_now_add=True, verbose_name='Дата регистрации')
+    email = models.EmailField(unique=True, verbose_name="email address")
+    sex = models.PositiveSmallIntegerField(choices=SEX, default=1, verbose_name="Пол")
+    role = models.PositiveSmallIntegerField(
+        choices=Role.choices, default=Role.Customer, verbose_name="кто по жизни"
+    )
+    phone = models.CharField(
+        max_length=25, blank=True, null=True, verbose_name="Телефон"
+    )
+    photo = models.ImageField(
+        upload_to="user.photos", blank=True, null=True, verbose_name="Фото"
+    )
+    registration_date = models.DateField(
+        auto_now_add=True, verbose_name="Дата регистрации"
+    )
 
     def __str__(self):
         return self.username
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-        ordering = ['id']
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["id"]
+
+
+class WorkerSchedule(models.Model):
+    DAYS_OF_WEEK = [
+        ("mon", "Понедельник"),
+        ("tue", "Вторник"),
+        ("wed", "Среда"),
+        ("thu", "Четверг"),
+        ("fri", "Пятница"),
+        ("sat", "Суббота"),
+        ("sun", "Воскресенье"),
+    ]
+
+    worker = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={"role": User.Role.Worker},
+        verbose_name="Специалист",
+        related_name="worker_schedule",
+    )
+    day_of_week = models.CharField(
+        max_length=3, choices=DAYS_OF_WEEK, verbose_name="День недели"
+    )
+    start_time = models.TimeField(verbose_name="Время начала")
+    end_time = models.TimeField(verbose_name="Время окончания")
+    appointment_duration = models.PositiveSmallIntegerField(
+        verbose_name="Длительность приема",
+        validators=[MinValueValidator(1), MaxValueValidator(120)],  # Продолжительность приема в минутах (от 1 до 120)
+    )
+
+    def __str__(self):
+        return f"{self.worker} - {self.get_day_of_week_display()}"
+
+    class Meta:
+        verbose_name = "Расписание специалиста"
+        verbose_name_plural = "Расписания специалистов"
