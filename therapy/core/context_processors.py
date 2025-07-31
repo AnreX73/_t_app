e474bf07ec5b60cc46bb2ce7c6c6490e307c47ee
AnreX73 <han220173@gmail.com>
2025-07-31 15:31:46 +0700

    change profile

--- therapy/core/context_processors.py
+++ therapy/core/context_processors.py
@@ -1,5 +1,5 @@
 from .models import AdditionalMaterials
 
 def get_logo(request):
-   logo = AdditionalMaterials.objects.filter(title="логотип").first()   
+   logo = AdditionalMaterials.objects.filter(title="logo").first()   
    return {"logo": logo}
